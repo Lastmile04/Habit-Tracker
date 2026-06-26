25404b04df78e2c09b57f314c3453704a6ad2ab2
Lastmile04 <thakurdikshant05@gmail.com>
2026-06-26 22:36:20 +0530

    fix: added dynamic week tracking

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import { Header } from "./components/Header"
 import { HabitForm } from "./components/HabitForm"
 import { HabitList } from "./components/HabitList"
 import { HabitProvider } from "./context/HabitProvider";
+import { useState } from "react";
+import { addWeeks, eachDayOfInterval, endOfWeek, startOfWeek } from "date-fns";
 
 export default function App() {
+    const [weekOffset, setWeekOffset] = useState(0);
+
+    const week = addWeeks(new Date(), weekOffset);
+    const visibleDates = eachDayOfInterval({
+        start: startOfWeek(week, { weekStartsOn: 1 }),
+        end: endOfWeek(week, { weekStartsOn: 1 }),
+    });
 
     return (
         // mx: margin on the left and right of auto
         <div className="max w-2xl mx-auto p-4 flex flex-col gap-4">
             <HabitProvider>
-                <Header />
+                <Header visibleDates={visibleDates} />
                 <HabitForm />
                 <HabitList />
             </HabitProvider>
         </div>
     )
 }
-
 
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,30 @@
+import { format, isToday } from "date-fns";
+import { useHabits } from "../context/useHabits"
 import { Button } from "./Buttons"
 
-export function Header() {
+type HeaderProps = {
+    visibleDates: Date[];
+}
+
+export function Header({ visibleDates }: HeaderProps) {
+    const { habits } = useHabits();
+
+    // gives an array of all the ones that are currently done  
+    const doneToday = habits.filter(h =>
+        h.completions.some(c => isToday(c)),
+    ).length;
+
+    const dateRange = `${format(visibleDates[0], "MMM dd")} - ${format(visibleDates.at(-1)!, "MMM dd")}`
     return (
         <header className="flex items-center justify-between">
             <div className="flex flex-col gap-1">
                 <h1 className="text-3xl font-bold">Habit tracker</h1>
-                <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+                <span className="text-zinc-400 text-sm">{doneToday}/{habits.length} </span>
 
             </div>
 
             <div className="flex flex-col gap-1 items-end">
-                <span className="text-zinc-400 text-sm"> Jun 15 - Jun 20 </span>
+                <span className="text-zinc-400 text-sm"> {dateRange}</span>
                 <div className="flex items-center gap-3">
                     <Button>Prev</Button>
                     <Button>Next</Button>
